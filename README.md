bc41645501bd720df3c23ad902e11ed700e0bea7
jaykaranja <mainakaranja2001@gmail.com>
2023-03-17 15:40:15 +0300

    Package to implement Azure CosmosDB functions from a React application.

---
+++ README.md
@@ -0,0 +1,13 @@
+The code above contains all the main functionalities required to manage an Azure CosmosDB resource that can be run from a ReactJS application. All credit goes to the author of the code, and Microsoft.
+For more information, simply visit https://www.npmjs.com/package/@azure/cosmos.
+
+To start, perform:
+
+npm install
+
+All the functions are contained in the src/cosmos/Client.js where each line is well documented for you to implement accordingly. In case of any problem
+ you run into, fix it. I am not liable of any wrong doing done by use of the function. Configure the structure of the Client.js file accordingly to avoid the code running in an unexpected manner before you run the start function.
+
+ Once your files are organized, perform:
+
+npm run dev
---
+++ src/cosmos/Client.js
@@ -0,0 +1,91 @@
+// This code shows the implementation of Azure CosmosDb using ReactJS
+// All credit belong to the author of the code as I am not the original author
+// For more information, visit https://www.npmjs.com/package/@azure/cosmos
+
+// This line imports the necessary resources required for all the functionality
+import { CosmosClient } from "@azure/cosmos"
+
+// Declaring the environment variables
+
+// This is your Azure CosmosDB account URI link
+const endpoint = "https://purchaseorderitems.documents.azure.com:443/";
+
+// This is the primary key to your Azure CosmosDB account
+const key = "UEyhDWw0UF9CweujkD8xlhtnhWpucIJHiElDrLa47gL77EwBfCMueYfeDcwiZPwvB3VyX6uignNBACDbPg1ohQ==";
+
+// Instantiating a CosmosClient object
+const client = new CosmosClient({ endpoint, key });
+
+export async function AzureCosmos() {
+    // This piece of code creates a database on Azure CosmosDB
+
+    const { database } = await client.databases.createIfNotExists({ id: "Family" });
+    console.log(database.id);
+
+    // End of code
+
+    // This piece of code creates a container on Azure CosmosDB
+
+    const { container } = await database.containers.createIfNotExists({ id: "Test Database" });
+    console.log(container.id);
+
+    // End of code
+
+    // This piece of code inserts items into Azure CosmosDB
+
+    const cities = [
+        { id: "1", name: "Olympia", state: "WA", isCapitol: true },
+        { id: "2", name: "Redmond", state: "WA", isCapitol: false },
+        { id: "3", name: "Chicago", state: "IL", isCapitol: false }
+      ];
+
+      for (const city of cities) {
+        await container.items.create(city);
+      }
+
+    // End of code
+
+    // This piece of code reads an item from  Azure CosmosDB
+
+    await container.item("1").read();
+
+    // End of code
+
+    // This piece of code deletes first item returned on Azure CosmosDB
+
+    await container.item("1").delete();
+
+    // End of code
+
+    // This piece of code queries the DB on Azure CosmosDB
+
+    const { resources } = await container.items
+    .query("SELECT * from c WHERE c.isCapitol = true")
+    .fetchAll();
+
+    for (const city of resources) {
+        console.log(`${city.name}, ${city.state} is a capitol `);
+    }
+
+    // End of code
+
+    // This piece of code performs parameterized queries on Azure CosmosDB
+
+    const { data } = await container.items
+    .query({
+        query: "SELECT * from c WHERE c.isCapitol = @isCapitol",
+        parameters: [{ name: "@isCapitol", value: true }]
+    })
+    .fetchAll();
+    
+    for (const city of data) {
+    console.log(`${city.name}, ${city.state} is a capitol `);
+    }
+
+    // End of code
+}
+
+// Error catching is done here
+AzureCosmos().catch((error) => {
+  console.error(error);
+});
